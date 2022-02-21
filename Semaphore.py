636616464391549aa99ef547675538f34b6b81b8
alejmira <alejandro.mirandacortes@gmail.com>
2022-02-21 19:28:15 +0100

    Lock.py es la solución usando Lock y Semaphore.py la que usa BoundedSemaphore(1)

---
+++ Semaphore.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 20 16:05:32 2022
+
+@author: alejandro
+"""
+
+from multiprocessing import Process
+from multiprocessing import current_process
+from multiprocessing import Value, Array
+from multiprocessing import BoundedSemaphore
+
+N = 8
+
+def task(semaphore, common, tid):
+    a = 0
+    for i in range(100):
+        print(f"{tid}−{i}: Non−critical Section", flush = True)
+        a += 1
+        print(f"{tid}−{i}: End of non−critical Section", flush = True)
+        semaphore.acquire()
+        try:
+            print(f"{tid}−{i}: Critical section", flush = True)
+            v = common.value + 1
+            print(f"{tid}−{i}: Inside critical section", flush = True)
+            common.value = v
+            print(f"{tid}−{i}: End of critical section", flush = True)
+        finally:
+            semaphore.release()
+    
+        
+def main():
+    semaphore = BoundedSemaphore(1)
+    lp = []
+    common = Value("i", 0)
+    for tid in range(N):
+        lp.append(Process(target=task, args=(semaphore, common, tid)))
+    print (f"Valor inicial del contador {common.value}", flush = True)
+    for p in lp:
+        p.start()
+    for p in lp:
+        p.join()
+    print (f"Valor final del contador {common.value}", flush = True)
+    print ("fin", flush = True)
+
+if __name__ == "__main__":
+    main()
